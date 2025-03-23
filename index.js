3baa24585ba3d63559b3253ec6401702ef059661
River Vora <vorariver@gmail.com>
2025-03-23 15:46:22 -0400

    Enable HTTPS support by configuring SSL certificates and updating server settings

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const https = require('https');
 const app = express();
-const port = 80;
+const port = 443;
+
+const sslOptions = {
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert'),
+};
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -21,7 +28,7 @@ app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'contact.html'));
 });
 
-// Create HTTP server
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server is running on http://0.0.0.0:${port}, You can access it via http://192.168.1.125/home`);
+// Create HTTPS server
+https.createServer(sslOptions, app).listen(port, '0.0.0.0', () => {
+    console.log(`HTTPS Server running on https://0.0.0.0:${port}, You can access it via https://192.168.1.125/home`);
 });
